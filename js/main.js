f36a28a8b2b84990f0a8eca4649ced7107f9ed0c
Rimantas <belauzas@yahoo.com>
2021-06-02 20:50:00 +0300

    Pomidoro uzduotis

--- js/main.js
+++ js/main.js
@@ -1,15 +1,13 @@
 /*
 Surasti h1 (antraste)
-Perrasyti h1 elemento teksta i "Zuikio darzas"
+Perrasyti h1 elemento teksta i "Zuikio pomidoru darzas"
+Padaryk, jog h1 elementas turetu CSS class "pavadinimas"
 
 Surasti visus darzoves
-Kiekvienos darzoves teksta pakeisti i "Israuta darzove"
+Kiekvienos darzoves teksta pakeisti i "Pomidoras"
+Kiekvienai darzovei suteikti CSS class "pomidoras"
 */
 
 const h1DOM = document.querySelector('h1');
-h1DOM.innerText = 'Zuikio darzas';
-
-const darzovesDOM = document.querySelectorAll('li');
-for (let i = 0; i < darzovesDOM.length; i++) {
-    darzovesDOM[i].innerText = 'Israuta darzove';
-}
+h1DOM.innerText = 'Zuikio pomidoras darzas';
+h1DOM.classList.add('pavadinimas');
