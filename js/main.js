eb89fc451d2d4c7e9904231183b5d0c08d3592d8
Rimantas <belauzas@yahoo.com>
2021-06-02 19:40:17 +0300

    Sriuba final

--- js/main.js
+++ js/main.js
@@ -4,12 +4,17 @@ Surasti visus ingredietus ir ju pavadinimus atspausdinti i console
 Visu ingredienu pavadinimai turi buti didziosiomis raidemis ir sudeti i array
 
 Isspausdinti gauta array i console
+
+Norimas rezultatas:
+['MORKOS', 'BULVES', 'SVOGUNAS', 'VANDUO', 'DRUSKA']
 */
 
 const liDOM = document.querySelectorAll('li');
-console.log(liDOM);
+const ingredientai = [];
 
 for (let i = 0; i < liDOM.length; i++) {
     const ingredientas = liDOM[i].innerText;
-    console.log(ingredientas);
+    ingredientai.push(ingredientas);
 }
+
+console.log(ingredientai);
