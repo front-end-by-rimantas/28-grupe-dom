0ee1eef062b9d906c95fa5af5465b8994dbdad08
Rimantas <belauzas@yahoo.com>
2021-06-03 20:18:26 +0300

    Zaidimas prasideda

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 let rezultatas = 0;
+
+const minusDOM = document.querySelector('.minus');
+const plusDOM = document.querySelector('.plus');
+const numberDOM = document.querySelector('.number');
+const resetDOM = document.querySelector('.reset');
+
+minusDOM.addEventListener('click', () => {
+    numberDOM.innerText = --rezultatas;
+})
+
+plusDOM.addEventListener('click', () => {
+    numberDOM.innerText = ++rezultatas;
+})
+
+resetDOM.addEventListener('click', () => {
+    rezultatas = 0;
+    numberDOM.innerText = rezultatas;
+})
