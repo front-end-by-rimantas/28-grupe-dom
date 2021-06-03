05ece99df61548ee6bb020287809b67633ad0a0e
Rimantas <belauzas@yahoo.com>
2021-06-03 18:41:26 +0300

    Meniu event listener

--- js/main.js
+++ js/main.js
@@ -1,20 +1,9 @@
-/*
-Surasti h1 (antraste)
-Perrasyti h1 elemento teksta i "Zuikio pomidoru darzas"
-Padaryk, jog h1 elementas turetu CSS class "pavadinimas"
+const logoDOM = document.querySelector('.logo');
+const hamburgerDOM = document.querySelector('.hamburger');
 
-Surasti visus darzoves
-Kiekvienos darzoves teksta pakeisti i "Pomidoras"
-Kiekvienai darzovei suteikti CSS class "pomidoras"
-*/
+console.log(logoDOM);
+console.log(hamburgerDOM);
 
-const h1DOM = document.querySelector('h1');
-h1DOM.innerText = 'Zuikio pomidoru darzas';
-h1DOM.classList.add('pavadinimas');
-
-const darzoveDOM = document.querySelectorAll('li');
-
-for (let i = 0; i < darzoveDOM.length; i++) {
-    darzoveDOM[i].innerText = 'Pomidoras';
-    darzoveDOM[i].classList.add('pomidoras');
-}
+hamburgerDOM.addEventListener('click', () => {
+    logoDOM.classList.toggle('big');
+})
