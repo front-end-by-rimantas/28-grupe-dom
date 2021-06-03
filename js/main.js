93bfa988ec22ca132a77ab4bf4d77e85c686d2ea
Rimantas <belauzas@yahoo.com>
2021-06-03 19:22:50 +0300

    Plius-minus zaidimo uzduotis

--- js/main.js
+++ js/main.js
@@ -1,8 +1,1 @@
-const logoDOM = document.querySelector('.logo');
-const hamburgerDOM = document.querySelector('.hamburger');
-const navDOM = document.querySelector('nav');
-
-hamburgerDOM.addEventListener('click', () => {
-    logoDOM.classList.toggle('big');
-    navDOM.classList.toggle('visible');
-})
+let rezultatas = 0;
