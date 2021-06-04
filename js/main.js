b34afcf9756ad3929a12c49c85024f7008d7aaea
Rimantas <belauzas@yahoo.com>
2021-06-04 18:48:47 +0300

    Message update

--- js/main.js
+++ js/main.js
@@ -4,16 +4,22 @@ const minusDOM = document.querySelector('.minus');
 const plusDOM = document.querySelector('.plus');
 const numberDOM = document.querySelector('.number');
 const resetDOM = document.querySelector('.reset');
+const h1DOM = document.querySelector('h1');
+const gameInProgressMessage = 'Žaidimas progrese';
+const initialH1Text = h1DOM.innerText;
 
 minusDOM.addEventListener('click', () => {
     numberDOM.innerText = --rezultatas;
+    h1DOM.innerText = gameInProgressMessage;
 })
 
 plusDOM.addEventListener('click', () => {
     numberDOM.innerText = ++rezultatas;
+    h1DOM.innerText = gameInProgressMessage;
 })
 
 resetDOM.addEventListener('click', () => {
     rezultatas = 0;
     numberDOM.innerText = rezultatas;
+    h1DOM.innerText = initialH1Text;
 })
