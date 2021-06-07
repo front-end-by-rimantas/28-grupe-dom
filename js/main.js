ee13e2ec08c7c478b84fb8a6818541b6f2e9fd74
Rimantas <belauzas@yahoo.com>
2021-06-07 21:13:27 +0300

    Uzeigeles galutinis sprendinys

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const nameDOM = document.querySelector('#customer');
 const sriubaDOM = document.querySelector('#sriuba');
 const pagrindinisPatiekalasDOM = document.querySelector('#patiekalas');
 const desertasDOM = document.querySelector('#desertas');
-const allGerimaiDOM = document.querySelectorAll('input[name="gerymas"]');
+const allGerimaiDOM = document.querySelectorAll('input[name="gerimas"]');
 
 function arNoriu(DOM) {
     return DOM.checked ? 'nori' : 'nenori';
@@ -21,5 +21,29 @@ function kurisPazymetas(DOMlist) {
 submitDOM.addEventListener('click', (e) => {
     e.preventDefault();
 
-    orderDOM.innerText = `Uzsakovas vardu ${nameDOM.value} ${arNoriu(sriubaDOM)} sriubos, ${arNoriu(pagrindinisPatiekalasDOM)} pagrindinio patiekalo, ${arNoriu(desertasDOM)} deserto ir ${kurisPazymetas(allGerimaiDOM)} yra pasirinktas gerymas.`;
+    const vardas = nameDOM.value.trim();
+    const pasirinktasGerimas = kurisPazymetas(allGerimaiDOM);
+    const sakinioPradzia = vardas ? 'vardu ' + vardas : 'nenudores vardo';
+    const sakinioPabaiga = pasirinktasGerimas ? pasirinktasGerimas + ' yra pasirinktas gerimas' : 'gerimo nepasirinko';
+
+    orderDOM.innerText = `Uzsakovas ${sakinioPradzia} ${arNoriu(sriubaDOM)} sriubos, ${arNoriu(pagrindinisPatiekalasDOM)} pagrindinio patiekalo, ${arNoriu(desertasDOM)} deserto ir ${sakinioPabaiga}.`;
 })
+
+/*
+PRADZIA:
+Uzsakovas vardu X nori....
+Uzsakovas nenurodes vardo nori....
+
+PABAIGA:
+ir X yra pasirinktas gerimas.
+ir gerimo nepasirinko.
+
+
+EXTRA:
+Uzsakovas vardu X nori sriubos, pagrindinio patiekalo, deserto ir [gerimas]
+Uzsakovas vardu X nori sriubos, pagrindinio patiekalo, bet nenori deserto ir [gerimas]
+Uzsakovas vardu X nori sriubos, bet nenori pagrindinio patiekalo, deserto ir [gerimas]
+Uzsakovas vardu X nenori sriubos, pagrindinio patiekalo, deserto ir [gerimas]
+
+
+*/
