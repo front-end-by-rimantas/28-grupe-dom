0a58b0ad676a4087811698e6b82852dbfe1ad1be
Rimantas <belauzas@yahoo.com>
2021-06-07 19:36:51 +0300

    Uzeigeles pirminis sprendinys

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
 const submitDOM = document.querySelector('button');
 const orderDOM = document.querySelector('.order');
+const nameDOM = document.querySelector('#customer');
+const sriubaDOM = document.querySelector('#sriuba');
+const pagrindinisPatiekalasDOM = document.querySelector('#patiekalas');
+const desertasDOM = document.querySelector('#desertas');
+const allGerimaiDOM = document.querySelectorAll('input[name="gerymas"]');
+
+function arNoriu(DOM) {
+    return DOM.checked ? 'nori' : 'nenori';
+}
+
+function kurisPazymetas(DOMlist) {
+    for (const itemDOM of DOMlist) {
+        if (itemDOM.checked) {
+            return itemDOM.value;
+        }
+    }
+}
 
 submitDOM.addEventListener('click', (e) => {
     e.preventDefault();
-    orderDOM.innerText = `Uzsakovas vardu [vardas] [nori/nenori] sriubos, [nori/nenori] pagrindinio patiekalo, [nori/nenori] deserto ir [gerymo pavadinimas] yra pasirinktas gerymas.`;
+
+    orderDOM.innerText = `Uzsakovas vardu ${nameDOM.value} ${arNoriu(sriubaDOM)} sriubos, ${arNoriu(pagrindinisPatiekalasDOM)} pagrindinio patiekalo, ${arNoriu(desertasDOM)} deserto ir ${kurisPazymetas(allGerimaiDOM)} yra pasirinktas gerymas.`;
 })
