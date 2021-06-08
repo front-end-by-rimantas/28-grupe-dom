355461568d27026b8fede805cca458ba316820fc
Rimantas <belauzas@yahoo.com>
2021-06-08 21:03:02 +0300

    Socials uzdavinys

--- js/main.js
+++ js/main.js
@@ -1,22 +1,7 @@
-function generuotiLenta(selector, count) {
-    const DOM = document.querySelector(selector);
-    if (!DOM) {
-        console.error('ERROR: nerastas elementas');
-        return false;
-    }
+function generuotiSocials() {
 
-    if (count < 0 ||
-        count % 1 !== 0) {
-        console.error('ERROR: skaicius turi buti teigiamas ir sveikasis');
-        return false;
-    }
-
-    let HTML = '';
-    for (let i = 1; i <= count; i++) {
-        HTML += `<div>${i}</div>`;
-    }
-
-    DOM.innerHTML = HTML;
 }
 
-generuotiLenta('.lenta', 8);
+const icons = ['facebook', 'twitter', 'linkedin'];
+
+generuotiSocials('.socials', icons)
