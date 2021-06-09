a7871d1b3f35df17e06b19baf759f43ef36feac1
Rimantas <belauzas@yahoo.com>
2021-06-09 20:50:38 +0300

    Header menu uzduotis

--- js/main.js
+++ js/main.js
@@ -1,45 +1,24 @@
-function generuotiSocials(selector, iconsList) {
-    // validation
-    if (typeof selector !== 'string' ||
-        selector === '') {
-        console.error('ERROR: selector turi buti ne tuscias stringas', selector, iconsList);
-        return false;
-    }
+function renderNav() {
 
-    const DOM = document.querySelector(selector);
-    if (!DOM) {
-        console.error('ERROR: pagal pateikta selector nepavyko rasti jokio DOM elemento');
-        return false;
-    }
-
-    if (!Array.isArray(iconsList) ||
-        iconsList.length === 0) {
-        console.error('ERROR: iconsList turi buti ne tuscias array');
-        return false;
-    }
-
-    // logic
-    let HTML = '';
-    const availableIcons = ['facebook', 'twitter', 'linkedin', 'instagram', 'pinterest'];
-
-    for (const icon of iconsList) {
-        if (typeof icon === 'string' &&
-            icon !== '' &&
-            availableIcons.includes(icon)) {
-            HTML += `<i class="fa fa-${icon}"></i>`;
-        }
-    }
-
-    // post-logic validation
-    if (HTML === '') {
-        console.error('ERROR: iconsList turi tureti bent viena elementa kuris buti ne tuscias tekstas');
-        return false;
-    }
-
-    // result return
-    DOM.innerHTML = HTML;
 }
 
-const icons = ['facebook', 'instagram', 'twitter', 'linkedin'];
+const menu = [
+    {
+        href: '#',
+        title: 'Home'
+    },
+    {
+        href: '#',
+        title: 'Services'
+    },
+    {
+        href: '#',
+        title: 'About us'
+    },
+    {
+        href: '#',
+        title: 'Contact us'
+    }
+];
 
-generuotiSocials('.socials', icons);
+renderNav('header', menu);
