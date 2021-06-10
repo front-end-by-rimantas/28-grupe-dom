0319e592e6d520eca6c66f2cf8446b9531af63cc
Rimantas <belauzas@yahoo.com>
2021-06-10 20:29:33 +0300

    Meniu sprendinys su daug papildomu variaciju

--- js/main.js
+++ js/main.js
@@ -1,24 +1,22 @@
-function renderNav() {
-
-}
-
 const menu = [
-    {
-        href: '#',
-        title: 'Home'
-    },
-    {
-        href: '#',
-        title: 'Services'
-    },
-    {
-        href: '#',
-        title: 'About us'
-    },
-    {
-        href: '#',
-        title: 'Contact us'
-    }
+    { href: './', title: 'Home' },
+    { href: './services', title: 'Services' },
+    { href: './about-us', title: 'About us' },
+    { href: './contact-us', title: 'Contact us' }
 ];
+
+function renderNav(selector, data) {
+    const DOM = document.querySelector(selector);
+    let HTML = '';
+
+    let i = 0;
+    while (i < data.length) {
+        const menuItem = data[i];
+        HTML += `<a href="${menuItem.href}">${menuItem.title}</a>`;
+        i++;
+    }
+
+    DOM.insertAdjacentHTML('beforeend', `<nav>${HTML}</nav>`);
+}
 
 renderNav('header', menu);
