d61779c911aa92c882523c2cc93cc117fd59440a
Rimantas <belauzas@yahoo.com>
2021-06-16 19:11:29 +0300

    Tortu sprendinys

--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
-function time(nuo, iki, trukme) {
-    iki = nuo > iki ? iki + 24 : iki;
-    for (let i = nuo * 60; i <= iki * 60; i += trukme) {
-        const m = i % 60;
-        const h = (i - m) / 60 % 24;
-        console.log(`${h < 10 ? '0' + h : h}:${m < 10 ? '0' + m : m}`);
-    }
+function tortoGabalas(tortuKiekis, zmoniuKiekis, tortoSvoris) {
+    const zmoniuPerTorta = Math.ceil(zmoniuKiekis / tortuKiekis);
+    return Math.floor(tortoSvoris / zmoniuPerTorta * 1000);
 }
 
-time(8, 11, 45);
-time(22, 1, 45);
+console.log(tortoGabalas(3, 17, 1)); // 166
