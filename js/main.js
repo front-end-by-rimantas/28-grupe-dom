c31821a7b741a01fde92c7d205c8b9f23b81b33d
Rimantas <belauzas@yahoo.com>
2021-06-16 19:07:31 +0300

    Visos paros laikrodzio sprendinys

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 function time(nuo, iki, trukme) {
+    iki = nuo > iki ? iki + 24 : iki;
     for (let i = nuo * 60; i <= iki * 60; i += trukme) {
-        const h = Math.floor(i / 60);
-        const m = i - h * 60;
-        console.log(`${h}:${m < 10 ? '0' + m : m}`);
+        const m = i % 60;
+        const h = (i - m) / 60 % 24;
+        console.log(`${h < 10 ? '0' + h : h}:${m < 10 ? '0' + m : m}`);
     }
 }
 
 time(8, 11, 45);
+time(22, 1, 45);
